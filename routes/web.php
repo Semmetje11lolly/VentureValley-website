14dcd31a5e05b7a5560a5d773146d369ef5018c7
Semmetje <semvandenende@gmail.com>
2025-12-10 22:19:14 +0100

    Added base layout with navigation and footer. Removed some default Laravel stuff that I won't be using.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +14,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/', [HomeController::class, 'show'])
+    ->name('home');
+
+require __DIR__ . '/auth.php';
